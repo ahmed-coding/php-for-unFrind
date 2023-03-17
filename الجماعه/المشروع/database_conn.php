4ab254ea2c5b4fbfa6ee04def0ce812ca863248a
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2023-03-18 00:06:47 +0300

    Update database_conn.php
    project الجماعه

--- الجماعه/المشروع/database_conn.php
+++ الجماعه/المشروع/database_conn.php
@@ -6,7 +6,7 @@ $user = 'root';
 $conn = mysqli_connect($server, $user, null);
 
 
-$sql = "CREATE DATABASE IF NOT EXISTS $database;";
+$sql = "CREATE DATABASE IF NOT EXISTS $database  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;;";
 
 if (mysqli_query($conn, $sql)) {
 } else {
@@ -17,22 +17,21 @@ mysqli_close($conn);
 
 $conn = mysqli_connect($server, $user, null, $database);
 
-$sql1 = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50), password VARCHAR(50), admin BIT)";
-$sql2 = "CREATE TABLE IF NOT EXISTS category (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), image_url VARCHAR(255), description VARCHAR(255))";
-$sql3 = "CREATE TABLE IF NOT EXISTS product (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), price FLOAT, category_id INT, image_url VARCHAR(255), qyt INT)";
-
-// Execute SQL statements
-if (mysqli_query($conn, $sql1)) {
+$sql = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50), password VARCHAR(50), admin BIT)";
+if (mysqli_query($conn, $sql)) {
 } else {
     echo "Error creating table users: " . mysqli_error($conn);
 }
+$sql = "CREATE TABLE IF NOT EXISTS category (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), image_url VARCHAR(255), description VARCHAR(255))";
 
-if (mysqli_query($conn, $sql2)) {
+if (mysqli_query($conn, $sql)) {
 } else {
     echo "Error creating table category: " . mysqli_error($conn);
 }
 
-if (mysqli_query($conn, $sql3)) {
+$sql = "CREATE TABLE IF NOT EXISTS product (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), price FLOAT, category_id INT, image_url VARCHAR(255), qyt INT)";
+
+if (mysqli_query($conn, $sql)) {
 } else {
     echo "Error creating table product: " . mysqli_error($conn);
 }
