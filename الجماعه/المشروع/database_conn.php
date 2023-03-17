7c6cf544b45a372a3791fecc9492652672caed66
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2023-03-18 00:15:53 +0300

    Update database_conn.php
    مشروع الجماعه

--- الجماعه/المشروع/database_conn.php
+++ الجماعه/المشروع/database_conn.php
@@ -20,18 +20,18 @@ $conn = mysqli_connect($server, $user, null, $database);
 $sql = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50), password VARCHAR(50), admin BIT)";
 if (mysqli_query($conn, $sql)) {
 } else {
-    echo "Error creating table users: " . mysqli_error($conn);
+    die("Error creating table users: " . mysqli_error($conn));
 }
 $sql = "CREATE TABLE IF NOT EXISTS category (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), image_url VARCHAR(255), description VARCHAR(255))";
 
 if (mysqli_query($conn, $sql)) {
 } else {
-    echo "Error creating table category: " . mysqli_error($conn);
+    die("Error creating table category: " . mysqli_error($conn));
 }
 
 $sql = "CREATE TABLE IF NOT EXISTS product (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), price FLOAT, category_id INT, image_url VARCHAR(255), qyt INT)";
 
 if (mysqli_query($conn, $sql)) {
 } else {
-    echo "Error creating table product: " . mysqli_error($conn);
+    die("Error creating table product: " . mysqli_error($conn));
 }
